e66b6f3e593fc5615dc871d99547534e291ebe5e
adypr <workady2015@gmail.com>
2024-02-25 11:32:27 +0300

    feat: add php-cs-fixer config

--- themes/btartp/footer.php
+++ themes/btartp/footer.php
@@ -13,17 +13,17 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'btartp' ) ); ?>">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'btartp')); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'btartp' ), 'WordPress' );
-				?>
+                /* translators: %s: CMS name, i.e. WordPress. */
+                printf(esc_html__('Proudly powered by %s', 'btartp'), 'WordPress');
+?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'btartp' ), 'btartp', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+/* translators: 1: Theme name, 2: Theme author. */
+printf(esc_html__('Theme: %1$s by %2$s.', 'btartp'), 'btartp', '<a href="http://underscores.me/">Underscores.me</a>');
+?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- themes/btartp/index.php
+++ themes/btartp/index.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<link rel="icon" type="image/svg+xml" href="/vite.svg" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Main page</title>
