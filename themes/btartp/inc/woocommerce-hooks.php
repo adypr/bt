5283dd9974873922ea6608ad2fa6574746fe4742
adypr <workady2015@gmail.com>
2024-03-08 16:17:32 +0300

    feat: ws4, add front-page template, disable woocommerce styles

--- themes/btartp/inc/woocommerce-hooks.php
+++ themes/btartp/inc/woocommerce-hooks.php
@@ -1,19 +1,3 @@
 <?php
 
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-add_action( 'woocommerce_before_main_content', function() {
-    echo '<div id="primary" class="container btartp-theme">';
-} );
-add_action( 'woocommerce_after_main_content', function() {
-    echo '</div>';
-} );
-
-// Change breadcrambs
-
-remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
-
-add_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 9 );
-
-add_action( 'woocommerce_sidebar', 'woocommerce_breadcrumb', 11 );
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
