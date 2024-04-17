2b416aa9f1978155da257bacf55b8c631c091fa3
adypr <workady2015@gmail.com>
2024-04-17 13:14:53 +0300

    feat: add cards hover effects

--- themes/btartp/template-parts/content-product.php
+++ themes/btartp/template-parts/content-product.php
@@ -4,11 +4,13 @@ global $product;
 
 <li <?php wc_product_class( 'work', $product ); ?>>
     <a href="<?php the_permalink(); ?>" class="work__link woocommerce-LoopProduct-link woocommerce-loop-product__link">
-        <?php echo btartp_thumb(); ?>
-        <?php echo do_shortcode('[ti_wishlists_addtowishlist loop=yes]') ?>
+        <div class="work__container">
+            <?php echo btartp_thumb(); ?>
+        </div>    
         <div class="work__description">
             <h3 class="work__title"><?php the_title(); ?></h3>
             <p class="work__price"><?php echo $product->get_price_html(); ?></p>
         </div>
+        <?php echo do_shortcode('[ti_wishlists_addtowishlist loop=yes]'); ?>
     </a>
 </li>
